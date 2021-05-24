6db5db302452df26af14c69929d19740ddefdda7
mskim <net94@naver.com>
2021-05-24 16:07:54 +0900

    ㅁㅁㅁ

--- src/main/java/spring5_webmvc_mybatis_study/config/ContextRoot.java
+++ src/main/java/spring5_webmvc_mybatis_study/config/ContextRoot.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class})
+@Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class, ContextTransaction.class})
 @ComponentScan(basePackages = {
 		  "spring5_webmvc_mybatis_study.mappers",
 		  "spring5_webmvc_mybatis_study.controller",
